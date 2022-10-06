6d1b3c0793bc3d7a28c339f0a5084ef4ef9f319b
smartprincipal <johnsonoluwayemibunmi@gmail.com>
2022-10-06 02:21:38 -0700

    I implemented my section 2 design

---
+++ src/Components/Card/card.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import './card.css'
+
+const card = () => {
+  return (
+    <div>
+
+    </div>
+  )
+}
+
+export default card
--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -3,6 +3,9 @@ import NavBar from '../../Components/NavBar/NavBar'
 import './LandingPage.css';
 import Button from '../../Components/Buttons/Button';
 import Tompurse from '../../Asset/TomPurse.png'
+import Invest from '../../Asset/Invest.svg'
+import Later from '../../Asset/Later.svg'
+import Global from '../../Asset/globalicon.svg' 
 
 const LandingPage = () => {
   return (
@@ -24,6 +27,34 @@ const LandingPage = () => {
         <h2>Your guide to financial wellness</h2>
         <p>We’ll help you invest, save and spend responsibly for just $1, $2 or $3 per month. No surprise fees, just surprise upgrades</p>
         <a className='learn' href="./">Learn More</a>
+      </div>
+      <div className="section2side2">
+        <div className="sec2">
+
+        <div className="section2side2inline">
+          <img src={Invest} alt="" />
+          <h4>Invest your spare change</h4>
+        </div>
+        <p className='sec2side2p'>Set aside the leftover change from everyday purchases by turning on automatic Round-Ups.</p>
+        </div>
+
+        <div className="sec2">
+
+        <div className="section2side2inline">
+          <img src={Later} alt="" />
+          <h4>Save for later</h4>
+        </div>
+        <p className='sec2side2p'>Set aside the leftover change from everyday purchases by turning on automatic Round-Ups.</p>
+        </div>
+
+        <div className="sec2exempt">
+
+        <div className="section2side2inline">
+          <img src={Global} alt="" />
+          <h4>Grow your knowledge</h4>
+        </div>
+        <p className='sec2side2p'>Set aside the leftover change from everyday purchases by turning on automatic Round-Ups.</p>
+        </div>
       </div>
      </section>
 
