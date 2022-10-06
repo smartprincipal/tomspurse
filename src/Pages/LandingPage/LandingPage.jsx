4e84eb8aac08d9c459ab8dab7738a56c28205bc7
smartprincipal <johnsonoluwayemibunmi@gmail.com>
2022-10-06 01:26:19 -0700

    I implemented Section2 of the Landing page

--- src/Pages/LandingPage/LandingPage.jsx
+++ src/Pages/LandingPage/LandingPage.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import NavBar from '../../Components/NavBar/NavBar'
 import './LandingPage.css';
 import Button from '../../Components/Buttons/Button';
+import Tompurse from '../../Asset/TomPurse.png'
 
 const LandingPage = () => {
   return (
@@ -11,7 +12,18 @@ const LandingPage = () => {
       <div className='section1Content'>
        <h3>Save Invest & Earn Anywhere !</h3>
        <p>Ipsum massa at cursus lacus erat lacus magna volutpat habitasse. Eu mauris et .</p>
-       <Button/>
+       <Button color='blue'/>
+      </div>
+      <div className="section1Img">
+        <img src={Tompurse} alt="tompurse" />
+      </div>
+     </section>
+
+     <section className="section2">
+      <div className="section2side1">
+        <h2>Your guide to financial wellness</h2>
+        <p>We’ll help you invest, save and spend responsibly for just $1, $2 or $3 per month. No surprise fees, just surprise upgrades</p>
+        <a className='learn' href="./">Learn More</a>
       </div>
      </section>
 
