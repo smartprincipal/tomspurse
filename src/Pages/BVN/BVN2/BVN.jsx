9604311d762e9b638cb2fbdcaeb907868504346f
smartprincipal <johnsonoluwayemibunmi@gmail.com>
2022-10-12 05:44:34 -0700

    Implemented some changes

--- src/Pages/BVN/BVN2/BVN.jsx
+++ src/Pages/BVN/BVN2/BVN.jsx
@@ -1,10 +1,25 @@
-import React from 'react'
+// import React, { useEffect } from 'react'
 import './BVN.css'
 import Dashboard from '../../Dashboard/Dashboard'
 import { Link } from 'react-router-dom'
 import Continue from '../../../Components/Buttons/Continue/Continue'
 
 const BVN = () => {
+// const [BVN , setBVN] = useState('')
+//   let Sandpoint= 'https://ravesandboxapi.flutterwave.com/v2/kyc/bvn/:bvnnumber?seckey=FLWSECK-e6db11d1f8a6208de8cb2f94e293450e-X'
+
+//   const bvnChange=(e) => {
+//     setBVN(e.target.value)
+//   }
+//   useEffect(() => {
+//     const bvnChange = async() =>{
+//       let response = await fetch('https://ravesandboxapi.flutterwave.com/v2/kyc/bvn/:bvnnumber?seckey=FLWSECK-e6db11d1f8a6208de8cb2f94e293450e-X')
+//       let data = await response.json()
+//       setData(JSON.stringify(data))
+//     }
+//     bvnChange()
+//   }, [BVN])
+  
   return (
     <div className='bvn'>
       <div className="dashB">
@@ -13,7 +28,13 @@ const BVN = () => {
       <div className="bvnVerification">
         <h4>Add your BVN</h4>
         <form action="" className='bvnForm'>
-          <input type="text" name="BVN" id="" className='bvnNumber' placeholder='Bank Verification Number' required/>
+          <input 
+          type="text" 
+          name="BVN" id="" 
+          className='bvnNumber' placeholder='Bank Verification Number' 
+          value={BVN}
+          onChange={bvnChange}
+          required/>
           <div className="DOBBlock">
           <label htmlFor="" className='DOB'>Date of Birth</label>
           <div className="DOBInput">
